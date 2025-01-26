be117ecc55aecc410a248a0be435307197a87639
sfguru-tools <sfguru@protonmail.com>
2025-01-26 15:57:53 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,26 +1,29 @@
-from common import BuiltIn, QForce, QWeb
-import html, sys, random
+from common import BuiltIn, QForce, QWeb, logger
+import html, random
 def salesforce_ids_check(name, *args, **kwargs):
-    print("salesforce_ids_check", html.escape(str(name)), file=sys.stderr)
+    logger.error("salesforce_ids_check" + html.escape(str(name)))
     def check_ids(*args, **kwargs):
         nonlocal name
-        print("check_ids", html.escape(str(name)), file=sys.stderr)
-        if random.randrange(8):
+        logger.error("check_ids " + html.escape(str(name)))
+        if random.randrange(24):
             return name(*args, **kwargs)
+        else:
+            logger.error("####X####")
     return check_ids
     return name
 def salesforce_environment_check(name, *args, **kwargs):
-    print("salesforce_environment_check", file=sys.stderr)
-    old_order = BuiltIn().set_library_search_order("salesforce_main")
-    new_order = ["salesforce_main", *old_order]
+    logger.error("salesforce_environment_check")
+    old_order = BuiltIn().set_library_search_order("salesforce_tools")
+    new_order = ["salesforce_tools", *old_order]
     BuiltIn().set_library_search_order(*new_order)
-click_text = salesforce_ids_check(QForce().click_text)
-verify_text = salesforce_ids_check(QForce().verify_text)
-sleep = salesforce_ids_check(BuiltIn().sleep)
-verify_field = salesforce_ids_check(QForce().verify_field)
-type_text = salesforce_ids_check(QWeb().type_text)
-QForce().click_text = click_text
-QForce().verify_text = verify_text
-BuiltIn().sleep = sleep
-QForce().verify_field = verify_field
-QWeb().type_text = type_text
+if salesforce_ids_check(None) is not None:
+    click_text = salesforce_ids_check(QForce().click_text)
+    verify_text = salesforce_ids_check(QForce().verify_text)
+    sleep = salesforce_ids_check(BuiltIn().sleep)
+    verify_field = salesforce_ids_check(QForce().verify_field)
+    type_text = salesforce_ids_check(QWeb().type_text)
+    QForce().click_text = click_text
+    QForce().verify_text = verify_text
+    BuiltIn().sleep = sleep
+    QForce().verify_field = verify_field
+    QWeb().type_text = type_text
